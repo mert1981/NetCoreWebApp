82bff9d84b9826787b2813314d69166e2852dda1
Mert Koç <mertkoc1981@gmail.com>
2024-04-30 20:56:21 +0300

    Updates have been made to the Register page.

--- NetCoreWebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ NetCoreWebApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -98,8 +98,14 @@ namespace NetCoreWebApp.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+            
+            [Required]
+            public int OgrenciNo { get; set; }
+            public string? Adres { get; set; }
+            public string? Fakulte { get; set; }
+            public string? Bolum { get; set; }
 
-           
+
         }
 
 
@@ -120,7 +126,10 @@ namespace NetCoreWebApp.Areas.Identity.Pages.Account
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                  
-              
+                user.OgrenciNo = Input.OgrenciNo;
+                user.Adres = Input.Adres;
+                user.Fakulte = Input.Fakulte;
+                user.Bolum = Input.Bolum;
 
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
