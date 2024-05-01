c14a29650d0ff12a785e1d052b07128464de0529
Mert Koç <mertkoc1981@gmail.com>
2024-05-01 15:08:09 +0300

    Recent changes have been made.

--- NetCoreWebApp/obj/Debug/net8.0/NetCoreWebApp.AssemblyInfo.cs
+++ NetCoreWebApp/obj/Debug/net8.0/NetCoreWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCoreWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82bff9d84b9826787b2813314d69166e2852dda1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b22fb8efe50f443ae4dc35499af201684c1cb61")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCoreWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCoreWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
